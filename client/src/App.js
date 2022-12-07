f0d24b5e1d16cffe1f0ecf9afedf96ac0c19c1d1
David Whipple <dwhipp88@gmail.com>
2022-12-06 19:47:19 -0700

    setup routes for Login and Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
-import './App.css';
+import React, { Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import React, { Fragment } from 'react';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import './App.css';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      {/* <section className="container"> */}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+      {/* </section> */}
+    </Fragment>
+  </Router>
 );
 
 export default App;
