3cb047846485f6f36c3ab7d297bb154c9cd8446d
David Whipple <dwhipp88@gmail.com>
2022-12-14 18:44:40 -0700

    rolled back dependencies, restarted client

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,25 @@
-import React, { Fragment, useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/PrivateRoute';
-//Redux
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken';
-
+import logo from './logo.svg';
 import './App.css';
 
-if (localStorage.token) {
-  setAuthToken(localStorage.token);
-}
-
-const App = () => {
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
-
+function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-          {/* <section className="container"> */}
-          <Alert />
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route exact path="/register" element={<Register />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route element={<PrivateRoute />}>
-              <Route
-                element={<Dashboard />}
-                exact
-                path="/dashboard"
-              />
-            </Route>
-          </Routes>
-          {/* </section> */}
-        </Fragment>
-      </Router>
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
