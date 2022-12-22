dbe983f01adc5055952714bf37d1d927ad0cb796
David Whipple <dwhipp88@gmail.com>
2022-12-22 16:51:00 -0700

    added dashboard logic to display correct profile data

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,9 +4,15 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
-const Navbar = ({ auth: { isAuthenticated, loading}, logout }) => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
+      <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user"></i>{' '}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
       <li>
         <Link onClick={logout} to="#!">
           <i className="fas fa-sign-out-alt"></i>
