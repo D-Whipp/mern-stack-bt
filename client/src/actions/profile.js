17f11917cb66e4507d7c59b855e6bfebf691e35d
David Whipple <dwhipp88@gmail.com>
2022-12-22 19:39:17 -0700

    added create profile logic

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -10,12 +10,65 @@ export const getCurrentProfile = () => async (dispatch) => {
 
     dispatch({
       type: GET_PROFILE,
-      payload: res.data
+      payload: res.data,
     });
   } catch (err) {
     dispatch({
       type: PROFILE_ERROR,
-      payload: { msg: err.response.statusText, status: err.response.status }
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
     });
   }
 };
+
+// Create or update profile
+export const createProfile =
+  (formData, history, edit = false) =>
+  async (dispatch) => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+
+      const res = await axios.post('/api/profile', formData);
+
+      dispatch({
+        type: GET_PROFILE,
+        payload: {
+          type: GET_PROFILE,
+          payload: res.data,
+        },
+      });
+
+      dispatch(
+        setAlert(
+          edit ? 'Profile Updated' : 'Profile Created',
+          'success'
+        )
+      );
+
+      if (!edit) {
+        history.push('/dashboard');
+      }
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) =>
+          dispatch(setAlert(error.msg, 'danger'))
+        );
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: err.response.statusText,
+          status: err.response.status,
+        },
+      });
+    }
+  };
