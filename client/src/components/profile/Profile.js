98d7029659eea9e4b668a4f1646b04459ce0c33c
David Whipple <dwhipp88@gmail.com>
2022-12-30 17:15:25 -0700

    created logic for displaying experience and education to profil

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -13,8 +15,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
   return (
     <Fragment>
@@ -32,6 +33,10 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
+            <div className='profile-grid my-1'>
+              <ProfileTop profile={profile} />
+              <ProfileAbout profile={profile} />
+            </div>
         </Fragment>
       )}
     </Fragment>
