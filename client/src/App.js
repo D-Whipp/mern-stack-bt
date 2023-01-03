ebf732fb3128ebcfcbbd965ab3e7304eff113218
David Whipple <dwhipp88@gmail.com>
2023-01-02 17:29:48 -0700

    Post Reducer, Action, & Initial Component

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -73,6 +74,11 @@ const App = () => {
                 exact
                 path="/add-education"
                 component={AddEducation}
+              />
+              <PrivateRoute
+                exact
+                path="/posts"
+                component={Posts}
               />
             </Switch>
           </section>
