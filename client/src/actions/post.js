0b3748824992f3a5a0e1da56e7d52c08a377789b
David Whipple <dwhipp88@gmail.com>
2023-01-04 17:42:22 -0700

    Created Add Comment logic

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -7,6 +7,8 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   ADD_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from './types';
 
 // Get posts
@@ -119,7 +121,6 @@ export const addPost = (formData) => async (dispatch) => {
   }
 };
 
-
 // Get post
 export const getPost = (id) => async (dispatch) => {
   try {
@@ -139,3 +140,58 @@ export const getPost = (id) => async (dispatch) => {
     });
   }
 };
+
+// Add Comment
+export const addComment = (postId, formData) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const res = await axios.post(
+      `/api/posts/comment/${postId}`,
+      formData,
+      config
+    );
+
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Comment Added', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
+    });
+  }
+};
+
+// Remove Comment
+export const removeComment =
+  (postId, commentId) => async (dispatch) => {
+    try {
+      await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+      dispatch({
+        type: REMOVE_COMMENT,
+        payload: commentId,
+      });
+
+      dispatch(setAlert('Comment Removed', 'success'));
+    } catch (err) {
+      dispatch({
+        type: POST_ERROR,
+        payload: {
+          msg: err.response.statusText,
+          status: err.response.status,
+        },
+      });
+    }
+  };
